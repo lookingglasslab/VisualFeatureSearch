394395ace345144c8c93366883713bd810418020
Devon Ulrich <dpulrich@gmail.com>
2022-01-11 16:56:00 -0500

    Added simple overlay elements

--- src/Highlight.svelte
+++ src/Highlight.svelte
@@ -2,4 +2,30 @@
     export let img_url;
     export let size = 256;
 </script>
-<img src="{img_url}" alt="basic example" width={size} height={size}>
+<div class="container" style="width: {size}px; height: {size}px;">
+    <div class="image" style="background-image: url('{img_url}'); width: {size}px; height: {size}px;"></div>
+    <div class="overlay" style="z-index: 10; width: {size}px; height: {size}px;"></div>
+</div>
+
+<style>
+    .container {
+        position: relative;
+        display: inline-block;
+    }
+
+    .image, .overlay {
+        position: absolute;
+        left: 0px;
+        top: 0px;
+    }
+
+    .overlay {
+        opacity: 0;
+        transition: opacity .1s ease-in-out;
+        background-color: white;
+    }
+
+    .overlay:hover {
+        opacity: 0.7;
+    }
+</style>
