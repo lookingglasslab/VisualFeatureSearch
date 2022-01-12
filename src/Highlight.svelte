03422fd870f8bace6a7208878fcd9581c68ee217
Devon Ulrich <dpulrich@gmail.com>
2022-01-11 19:21:50 -0500

    Created simple highlighting functionality

--- src/Highlight.svelte
+++ src/Highlight.svelte
@@ -1,10 +1,36 @@
 <script>
+    import { onMount } from "svelte";
+
     export let img_url;
     export let size = 256;
+    export let highlightRadius = 15;
+
+    let canvas;
+    let ctx;
+
+    onMount(() => {
+        canvas.width = size;
+        canvas.height = size;
+        ctx = canvas.getContext('2d');
+    });
+
+    function handleMove(e) {
+        ctx.fillStyle = '#fff';
+        ctx.beginPath();
+        ctx.arc(e.offsetX, e.offsetY, highlightRadius, 0, 2 * Math.PI);
+        ctx.fill();
+    }
 </script>
+
 <div class="container" style="width: {size}px; height: {size}px;">
     <div class="image" style="background-image: url('{img_url}'); width: {size}px; height: {size}px;"></div>
-    <div class="overlay" style="z-index: 10; width: {size}px; height: {size}px;"></div>
+
+    <canvas 
+        bind:this={canvas}
+        on:mousemove={handleMove} 
+        class="overlay" 
+        style="z-index: 10; width: {size}px; height: {size}px;"
+    ></canvas>
 </div>
 
 <style>
@@ -22,7 +48,6 @@
     .overlay {
         opacity: 0;
         transition: opacity .1s ease-in-out;
-        background-color: white;
     }
 
     .overlay:hover {
