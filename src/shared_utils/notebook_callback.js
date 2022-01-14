578421d3b08144d40a981df5ec76177860d7719b
Devon Ulrich <dpulrich@gmail.com>
2022-01-13 19:14:07 -0500

    Added Colab support for notebook callbacks
    The notebook needs a few more steps to receive the invocation.
    See https://colab.research.google.com/notebooks/snippets/advanced_outputs.ipynb

--- src/shared_utils/notebook_callback.js
+++ src/shared_utils/notebook_callback.js
@@ -1,7 +1,11 @@
+// connects to the Python kernel for the notebook in use,
+// and runs the function function_name("data").
+// Only works in Google Colab and Jupyter Notebook environments.
 export function execute(function_name, data) {
     if (typeof Jupyter !== 'undefined') {
-        // TODO: add support for Colab as well
         let call_statement = function_name + '("' + data + '")';
         Jupyter.notebook.kernel.execute(call_statement);
+    } else if (typeof google !== 'undefined') {
+        google.colab.kernel.invokeFunction(function_name, [data], {});
     }
 }
