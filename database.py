ebac55879d31089dac5fd61fe6b0b1ec04c5f30a
Devon Ulrich <dpulrich@gmail.com>
2022-02-11 02:35:04 +0000

    Created Torch dataset for search database

---
+++ database.py
@@ -0,0 +1,46 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+from torchvision.io import read_image
+
+from PIL import Image
+import numpy as np
+
+import os
+
+# some utility transforms for us to prepare our data
+# from https://github.com/pytorch/examples/blob/master/imagenet/main.py#L227
+net_transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(), # note to future self: cannot be used for masks
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
+vis_transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224)
+])
+
+class SearchSpaceDataset(Dataset):
+    def __init__(self, path, transform=net_transform):
+        self._path = path
+        self._all_images = os.listdir(path)
+        self.transform = net_transform
+    
+    def __len__(self):
+        return len(self._all_images)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self._path, self._all_images[idx])
+        image = Image.open(img_path)
+
+        # convert grayscale to color, if necessary
+        color_image = Image.new('RGB', image.size)
+        color_image.paste(image)
+        image = color_image
+            
+        if self.transform:
+            image = self.transform(image)
+        return image, idx
