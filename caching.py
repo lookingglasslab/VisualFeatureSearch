ed688ac0a86593f2e17c318436b503e8659b9822
Devon Ulrich <dpulrich@gmail.com>
2022-06-01 00:24:06 +0000

    Changed caching method to support labelled datasets

--- caching.py
+++ caching.py
@@ -11,7 +11,7 @@ from torchvision import models, transforms
 
 import database
 import zarr
-
+ 
 def precompute(dataloader: DataLoader, model, cache_path, array_name):
     if not torch.cuda.is_available():
         raise Exception('No GPU Available')
@@ -20,7 +20,7 @@ def precompute(dataloader: DataLoader, model, cache_path, array_name):
     # model = model.to(gpu)
 
     # get output dimensions
-    img0 = dataloader.dataset[0]
+    img0 = dataloader.dataset[0][0]
     img0 = img0.to(gpu)
     output0 = model(img0[None, :, :, :])
     tmpfs = output0.shape
@@ -38,7 +38,7 @@ def precompute(dataloader: DataLoader, model, cache_path, array_name):
         it = iter(dataloader)
         idx = 0
         for batch in it:
-            batch = batch.to(gpu)
+            batch = batch[0].to(gpu)
             features = model(batch)
             features = features.cpu().numpy()
             length = min(dataloader.batch_size, len(out_feats) - idx)
